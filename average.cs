b0675618c52a5f535f5137c73b41cff7b2bbf13e
zabaltxuki <74701712+zabaltxuki@users.noreply.github.com>
2020-11-20 23:21:15 +0100

    Add files via upload

---
+++ average.cs
@@ -0,0 +1,30 @@
+// program that calculates the average of a set of marks until the user types "end"
+
+using System;
+
+public class Average
+{
+	public static void Main()
+	{
+		double a=0, b, c=0, d;
+		string s;
+		
+		Console.Write("Insert a mark: ");
+		s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
+		
+		while(s!="end")  // if the mark isn't an end then we pass it to double to work with it
+		{ 
+			b=Convert.ToDouble(s);
+			c++;   //counter of numbers
+			a+=b;  // sum of all the notes
+			Console.Write("Insert a mark: ");
+			s=Console.ReadLine();
+		}
+		if (c != 0)
+		{
+			d = a / c;  //calculate the average
+		}
+		Console.WriteLine("The average is {0}.",d);
+	}
+}
+
